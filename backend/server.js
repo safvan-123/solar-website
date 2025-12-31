6179b903c2ddb3910632c8a5e5a6504cd9907233
sahilgitt <sahilshabeer117@gmail.com>
2025-12-31 23:23:54 +0530

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://your-frontend.vercel.app",
+      "https://solar-website-topaz.vercel.app/",
     ],
     credentials: true,
   })

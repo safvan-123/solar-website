f32d4424f584b2af12e4e2a94560486ba8fd3ad6
sahilgitt <sahilshabeer117@gmail.com>
2026-01-03 12:59:04 +0530

    new backend changes

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,33 +1,3 @@
-// import Admin from "../models/Admin.js";
-// import bcrypt from "bcryptjs";
-// import jwt from "jsonwebtoken";
-
-// export const loginAdmin = async (req, res) => {
-//   console.log(req.body);
-
-//   try {
-//     const { email, password } = req.body;
-
-//     const admin = await Admin.findOne({ email });
-//     console.log(admin);
-
-//     if (!admin) return res.status(401).json({ message: "Invalid credentials" });
-
-//     const isMatch = await bcrypt.compare(password, admin.password);
-//     console.log(isMatch);
-//     if (!isMatch)
-//       return res.status(401).json({ message: "Invalid credentials" });
-
-//     const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET, {
-//       expiresIn: "1d",
-//     });
-
-//     res.json({ token });
-//   } catch (err) {
-//     res.status(500).json({ message: "Server error" });
-//   }
-// };
-
 import Admin from "../models/Admin.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
